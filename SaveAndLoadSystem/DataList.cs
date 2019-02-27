2813005d0b53e8ea013f9ab9ea16eea3b6f90288
Odeyalko <dmaximoffne@mail.ru>
2019-02-27 22:49:05 +0400

    addClass

---
+++ SaveAndLoadSystem/DataList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaveAndLoadSystem
+{
+    class DataList
+    {
+        List<FormOfData> workers;
+
+        public DataList()
+        {
+            workers = new List<FormOfData>();
+        }
+
+        public void Add(string name, int age, string job)
+        {
+            workers.Add(new FormOfData(name, age, job));
+        }
+
+        public void PrintList()
+        {
+            foreach (var e in workers)
+            {
+                Console.WriteLine(e.ToString()); 
+            }
+            
+        }
+        
+    }
+}
--- SaveAndLoadSystem/Program.cs
+++ SaveAndLoadSystem/Program.cs
@@ -10,6 +10,16 @@ namespace SaveAndLoadSystem
     {
         static void Main(string[] args)
         {
+
+            DataList d = new DataList();
+            d.Add("Rob", 31, "Programmer");
+            d.Add("Get", 31, "Modeller");
+            d.Add("Freeman", 31, "Httpator");
+            d.Add("Robinson", 31, "humanizator");
+            d.PrintList();
+
+
+            Console.ReadKey();
         }
     }
 }
